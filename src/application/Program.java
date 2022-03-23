83bb29bc93bea9dde7263d1758b21d0f7d3db74a
Francisco Gleriston da Oliveira <88321125+FranckOliveira@users.noreply.github.com>
2022-03-22 21:04:56 -0300

    Add files via upload
    Primeira versão da solução para o exercício proposto.

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Estudante;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Estudante[] vect = new Estudante[10];
+		
+		System.out.println("Quantos quartos serão alugados?");
+		int q = sc.nextInt();
+		
+		for (int i=0; i < q; i++) {
+			System.out.println();
+			System.out.println("Locador nº"+i+":");
+			System.out.println("Nome do locador: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("E-mail do locador: ");
+			String email = sc.nextLine();
+			System.out.println("Quarto?");
+			int quarto = sc.nextInt();
+			vect [quarto] = new Estudante(nome, email);
+		}
+		
+		System.out.println();
+		System.out.println("Quartos ocupados: ");
+		for (int i=0; i<10;i++) {
+			if(vect[i] != null) {
+				System.out.println(i+":"+vect[i]);
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
